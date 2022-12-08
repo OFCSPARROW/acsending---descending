74843deeee1824efb958d1c925e49779a5b20353
OFC SPARROW <110273928+OFCSPARROW@users.noreply.github.com>
2022-12-08 12:07:15 +0530

    acsending and descending
    this is js task

---
+++ ASCENDING DESCENDING/js/ad.js
@@ -0,0 +1,16 @@
+let as=[1,2,3,4,5,6,7,8,9];
+console.log("asceding   =" + as);
+
+let de1=as.pop();
+let de2=as.pop();
+let de3=as.pop();
+let de4=as.pop();
+let de5=as.pop();
+let de6=as.pop();
+let de7=as.pop();
+let de8=as.pop();
+let de9=as.pop();
+
+let de=[];
+de.push(de1,de2,de3,de4,de5,de6,de7,de8,de9);
+console.log("descending =" + de);
